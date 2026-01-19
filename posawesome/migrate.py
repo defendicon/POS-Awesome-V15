b8a5372e7ff6fc47c8dc8490c54e23c0c0446c14
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2026-01-19 21:29:42 +0500

    Reset custom field fixtures during migrate

--- posawesome/migrate.py
+++ posawesome/migrate.py
@@ -4,7 +4,26 @@ from frappe.utils.fixtures import sync_fixtures
 from posawesome.uninstall import clear_custom_fields_and_properties
 
 
+def _delete_posawesome_custom_fields():
+    custom_fields = frappe.db.get_all(
+        "Custom Field",
+        filters={"name": ("like", "%-posa_%")},
+        pluck="name",
+    )
+    for name in custom_fields:
+        frappe.db.delete("Custom Field", {"name": name})
+
+    property_setters = frappe.db.get_all(
+        "Property Setter",
+        filters={"name": ("like", "%-posa_%")},
+        pluck="name",
+    )
+    for name in property_setters:
+        frappe.db.delete("Property Setter", {"name": name})
+
+
 def reset_custom_fields_on_migrate():
     """Remove POS Awesome custom fields/properties then re-apply fixtures."""
     clear_custom_fields_and_properties()
+    _delete_posawesome_custom_fields()
     sync_fixtures("posawesome")
