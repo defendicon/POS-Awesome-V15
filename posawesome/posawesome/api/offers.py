c57a45d06bf157d8a4b76e09332434e3dbf2fdac
Defendicon <robin001100110@gmail.com>
2026-04-29 16:44:36 +0500

    fix: remove non-existent row_id column from POS Offer SELECT query

--- posawesome/posawesome/api/offers.py
+++ posawesome/posawesome/api/offers.py
@@ -66,7 +66,7 @@ def get_offers(profile):
     data = (
         frappe.db.sql(
             """
-        SELECT name, row_id, offer_applied, auto, min_qty, max_qty, min_amt, max_amt, discount_type, rate, discount_amount, discount_percentage
+        SELECT name, offer_applied, auto, min_qty, max_qty, min_amt, max_amt, discount_type, rate, discount_amount, discount_percentage
         FROM `tabPOS Offer`
         WHERE
         disable = 0 AND
